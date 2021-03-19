aba95051d89eff596a235605f8069d595ce8044e
Ayushi Jain <ayujain1109@gmail.com>
2021-03-19 22:31:51 +0530

    progress added

--- task/urls.py
+++ task/urls.py
@@ -5,10 +5,12 @@ urlpatterns=[path("",views.home,name="homepage"),
             path("userform", views.userlogin, name="user"),
             path("registration", views.register, name="register"),
             path("userprofile", views.userprofile, name="userprofile"),
-path("MeetTheTeam", views.Team, name="events"),
-path("disease", views.disease, name="disease"),
-path("helpline",views.helpline,name="expert"),
-path("update/<int:pk>/", views.update, name="update"),
-path("day_challenge",views.challenge,name="challenge"),
-             path("doctor",views.doctor,name="doctor")
+            path("MeetTheTeam", views.Team, name="events"),
+            path("disease", views.disease, name="disease"),
+            path("helpline",views.helpline,name="expert"),
+            path("update/<int:pk>/", views.update, name="update"),
+            path("day_challenge",views.challenge,name="challenge"),
+            path("doctor",views.doctor,name="doctor"),
+            path("progress", views.progress, name="progress"),
+
              ]
